607df61f9fafaaa92e8cffebc76cc7de3ce22c96
DnecLv <DnecLv@outlook.com>
2019-05-30 01:08:38 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var app = new Vue({
+var data = { acc: 'good' }
+
+// Object.freeze(data) 这会阻止传值
+
+var exp = new Vue({
     el: '#app',
     data: {
+        data,
         name: 'Dnec',
-    }
+    },
+    created: function () {
+        // `this` 指向 vm 实例
+        console.log('a is: ' + this.data)}
 });
+// vm.a == data.a 
